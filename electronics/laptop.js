d75f9422c5ae46a3e401798932184a87fa3799b5
Reetesh Shrivastava <reeteshshruvastava19@gmail.com>
2023-03-09 19:07:46 +0530

    electronics Page Done

---
+++ electronics/laptop.js
@@ -0,0 +1,36 @@
+var container = document.getElementById("dataDiv");
+let inp;
+ document.getElementById("lth").addEventListener("click",()=>{
+    inp = "asc";
+    getData(inp);
+ })
+ document.getElementById("htl").addEventListener("click",()=>{
+    inp = "desc";
+    getData(inp);
+ })
+
+async function getData(inp){
+    try {
+        var res = await fetch(`http://localhost:3000/electronicsData?key=laptop&_sort=price&_order=${inp}`);
+        var jsonData =await res.json();
+        display(jsonData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData();
+
+function display(data){
+    container.textContent = "";
+    data.forEach(elm=>{
+        var anchor = document.createElement("a");
+        var image = document.createElement("img");
+        image.src = elm.image;
+        var div = document.createElement("div");
+        div.textContent = elm.description;
+        var price = document.createElement("h4");
+        price.textContent = "$ "+ elm.price;
+        anchor.append(image,div,price);
+        container.append(anchor);
+    })
+}
